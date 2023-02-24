a7de10056882adf22b5de2615871ad0be2a1bfa1
Roberto Chasipanta <maniac787@gmail.com>
2023-02-24 09:18:50 -0500

    feat: steep 12  - Global error handler

---
+++ src/main/java/ec/com/nwi/springaws/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package ec.com.nwi.springaws.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = ErrorDetails.builder().timestamp(LocalDateTime.now()).message(
+                ex.getMessage()).details(request.getDescription(false)).build();
+
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = ErrorDetails.builder().timestamp(LocalDateTime.now()).message(
+                ex.getMessage()).details(request.getDescription(false)).build();
+
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
+}
---
+++ src/main/java/ec/com/nwi/springaws/exceptions/ErrorDetails.java
@@ -0,0 +1,19 @@
+package ec.com.nwi.springaws.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+}
