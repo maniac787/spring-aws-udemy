f9b2957d7d8f7c4a7a26a9f08b03830e919a88bb
Roberto Chasipanta <maniac787@gmail.com>
2023-02-24 11:09:02 -0500

    feat: steep 35  - Internationalization

--- src/main/java/ec/com/nwi/springaws/rest/HelloWorldController.java
+++ src/main/java/ec/com/nwi/springaws/rest/HelloWorldController.java
@@ -1,12 +1,22 @@
 package ec.com.nwi.springaws.rest;
 
 import ec.com.nwi.springaws.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "hello-world")
     public String helloWorld() {
         return "Hello world";
@@ -20,5 +30,21 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+        //return "Hello World V2";
+
+        //1:
+        //2:
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
     }
 }
